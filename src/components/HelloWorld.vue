a9d7a40c31fded913b64a1748ed10ccbf2f29105
alabo-excel <iamalaboexcel@gmail.com>
2020-12-21 07:57:08 +0100

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,146 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container>
+    <v-app-bar app color="#03081a" dark>
+      <v-row>
+        <v-col cols="3" class="d-flex">
+          <v-text-field v-model="name" label="Enter Name" ></v-text-field>
+          <v-btn @click="searchName">Search</v-btn>
+        </v-col>
+      </v-row>
+    </v-app-bar>
+
+    <v-row class="" justify="center">
+      <v-col cols="11"> </v-col>
+
+      <v-col cols="11" v-for="uni in unis" :key="uni.name">
+        <v-card id="card" class="mx-auto" max-width="800" outlined>
+          <v-list-item three-line>
+            <v-list-item-content>
+              <v-list-item-title class="headline mb-1 text-white">
+                {{ uni.name }} ({{ uni.acronym }})</v-list-item-title
+              >
+              <v-list-item-subtitle>{{ uni.motto }}</v-list-item-subtitle>
+            </v-list-item-content>
+
+            <v-list-item-avatar tile width="80" class="mt-9">
+              <img :src="uni.logo" alt="" />
+            </v-list-item-avatar>
+          </v-list-item>
+
+          <v-card-actions>
+            <a :href="uni.web">
+              <v-btn>Visit</v-btn>
+            </a>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+
+    <v-col col="12">
+      <div class="text-center">
+        <v-btn @click="next"><v-icon>mdi-chevron-left</v-icon></v-btn>
+        <v-btn @click="prev"><v-icon>mdi-chevron-right</v-icon></v-btn>
+      </div>
+    </v-col>
+
+    <div class="text-center ma-2">
+      <v-snackbar v-model="snackbar">
+        University not found
+        <template v-slot:action="{ attrs }">
+          <v-btn color="pink" text v-bind="attrs" @click="snackbar = false">
+            Close
+          </v-btn>
+        </template>
+      </v-snackbar>
+    </div>
+  </v-container>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+  name: "HelloWorld",
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+  data: () => ({
+    unis: null,
+    name: null,
+    snackbar: false,
+    index: 0,
+    allData: []
+  }),
+
+  mounted() {
+    fetch("https://nigerian-universities.herokuapp.com/")
+      .then((response) => response.json())
+      .then((data) => {
+        this.allData = data;
+        this.unis = data;
+        const n = 26;
+        const result = new Array(Math.ceil(this.unis.length / n))
+          .fill()
+          .map(() => this.unis.splice(0, n));
+        this.unis = result[this.index];
+      });
+  },
+  methods: {
+    searchName() {
+      console.log(this.allData)
+      var outputs = this.allData;
+      var final = [];
+      outputs.forEach((number) => {
+        if (number.name.includes(this.name)) {
+          final.push(number);
+          this.unis = final;
+        } else {
+          // this.snackbar = true;
+        }
+      });
+    },
+    next (){
+      this.index = this.index +1;
+      fetch("https://nigerian-universities.herokuapp.com/")
+      .then((response) => response.json())
+      .then((data) => {
+        this.unis = data;
+        const n = 26;
+        const result = new Array(Math.ceil(this.unis.length / n))
+          .fill()
+          .map(() => this.unis.splice(0, n));
+        this.unis = result[this.index];
+      });
+    },
+    prev(){
+      if(this.index > 0){
+        this.index = this.index-1;
+        fetch("https://nigerian-universities.herokuapp.com/")
+      .then((response) => response.json())
+      .then((data) => {
+        this.unis = data;
+        const n = 26;
+        const result = new Array(Math.ceil(this.unis.length / n))
+          .fill()
+          .map(() => this.unis.splice(0, n));
+        this.unis = result[this.index];
+      });
+      }else{
+        console.log("bad")
+      }
+    },
+  },
+    computed:{
+    
+    }
+};
+</script>
+<style>
 a {
-  color: #42b983;
+  text-decoration: none !important;
 }
+
+/* 
+  #app{
+    background-color: #03081a!important;
+  }
+  #card{
+    background-color: #03081a!important;
+  } */
 </style>
